79ba08516ce0cbded588bb975b07628fc8955aca
JLagT <jrl6253@psu.edu>
2025-05-25 00:33:29 -0600

    feat(frontend): App is now ready for UI development

---
+++ frontend/src/App.jsx
@@ -0,0 +1,63 @@
+const { useEffect } = require("react");
+//import React, { useState } from "react";
+import React from "react";
+import './styles.css';
+const path = require('path');
+const fs = require('fs');
+const actions = window.require('../renderer/actions.js');
+
+//========================== GLOBAL VARIABLES ==========================//
+const bindingsPath = path.join(__dirname, 'bindings.json');
+
+// const actionList = [
+//     "toggleMute",
+//     "playPause",
+//     "nextTrack",
+//     "previousTrack"
+// ];
+
+//========================== HELPER FUNCTIONS ==========================//
+
+function handlePacket(event) {
+    //transform JSON gesture to a string
+    const data = JSON.parse(event.data);
+    const gesture = data.gesture;
+    console.log("Received gesture:", gesture);
+    //get the newest bindings
+    const bindings = JSON.parse(fs.readFileSync(bindingsPath, 'utf8'));
+    const action = bindings[gesture];
+    //perform the action mapped by the gesture
+    actions.performAction(action);
+}
+
+//========================== MAIN FUNCTION ==========================//
+function App() {
+    //const [bindings, SetBindings] = useState({});
+    //Websocket response
+    useEffect(() => {
+        //1 create websocket
+        const socket = new WebSocket('ws://localhost:8765');
+        //2 handle incoming packets
+        socket.onmessage = (event) => {
+            handlePacket(event);
+        };
+        //3 handle unmount
+        return () => {
+            socket.close();
+        };
+    }, []);
+    //Binding mapping update
+    // useEffect(() => {
+    //     SetBindings(bindingsPath);
+    //     const parsedBindings = JSON.parse(fs.readFileSync(bindingsPath, 'utf8'));
+    //     SetBindings(parsedBindings);
+    // }, []);
+
+    return (
+        <h1 className="text-2xl font-bold text-purple-500">
+            Eyefred is running 🎉
+        </h1>
+    );
+}
+
+export default App;
