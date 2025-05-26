5c60960e10bf7e637ec34eae6d2e18a9953088c8
JLagT <jrl6253@psu.edu>
2025-05-26 01:49:37 -0600

    [feat]: we can now lower and raise volume with gestures!!!

--- backend/actions.js
+++ backend/actions.js
@@ -1,13 +1,28 @@
 const { exec } = require('child_process');
 
-module.exports = {
-    performAction(actionName) {
-        console.log(`(real) performAction called with: ${actionName}`);
-        if (actionName === 'playSpotify') {
-            exec('open -a Spotify'); // macOS example
-        } else if (actionName === 'mute') {
-            exec('osascript -e "set volume output muted true"');
-        }
-        // Add more actions here
+function performAction(actionName) {
+    console.log(`(real) performAction called with: ${actionName}`);
+    if (actionName === 'playSpotify') {
+        exec('open -a Spotify'); // macOS example
+    } else if (actionName === 'volumeDown') {
+        exec(
+            "osascript -e 'set cur to output volume of (get volume settings)' " +
+            "-e 'set tgt to cur - 10' " +
+            "-e 'if tgt < 0 then set tgt to 0' " +
+            "-e 'set volume output volume tgt'",
+            (err) => { if (err) console.error(err) }
+        );
     }
+    else if (actionName === 'volumeUp') {
+        "osascript -e 'set cur to output volume of (get volume settings)' " +
+            "-e 'set tgt to cur + 10' " +
+            "-e 'if tgt > 100 then set tgt to 100' " +
+            "-e 'set volume output volume tgt'",
+            (err) => { if (err) console.error(err) }
+    }
+    // Add more actions here
 }
+
+module.exports = {
+    performAction
+};
