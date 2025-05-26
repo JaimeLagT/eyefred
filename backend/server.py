073b36e1403eb46f5f602534776ede189db1beae
JLagT <jrl6253@psu.edu>
2025-05-25 18:46:57 -0600

    [docs]: small comment

--- backend/server.py
+++ backend/server.py
@@ -17,7 +17,8 @@ print("SERVER.PY STARTED")
 
 async def gesture_server(websocket, path):
     print("Entered gesture_server")
-
+    
+    #open camera
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Error: Could not open webcam.")
