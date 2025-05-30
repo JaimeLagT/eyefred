a734c2c298be184499257b4b52f978fc606686e3
JLagT <jlagunase@gmail.com>
2025-05-29 21:50:54 -0600

    [minor]: removed pirnt debug statement

--- backend/server.py
+++ backend/server.py
@@ -47,7 +47,6 @@ async def gesture_server(websocket, path):
 
                 #3 if a hand was detected pass them to detect gesture function 
                 gesture_name = None
-                print(lastFiredGesture)
                 if hasattr(results, "multi_hand_landmarks") and results.multi_hand_landmarks:
                     if (results.multi_handedness[0].classification[0].label == "Right"):
                         continue
