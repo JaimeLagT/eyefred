5be7d65b4f6d49127742fc4f3d0059d85502d891
JLagT <jlagunase@gmail.com>
2025-05-30 21:12:15 -0600

    [feat]: windows packaging update

--- frontend/renderer/vite.config.js
+++ frontend/renderer/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
-  base: './',  // <<< add this
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist'  // relative to renderer folder
+  }
 });
